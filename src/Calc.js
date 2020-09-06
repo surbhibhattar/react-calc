53b2db88e5efb6e21c9125190a95fb93137a189e
Surbhi <surbhibhattar92@gmail.com>
2020-09-06 11:11:08 +0530

    added a heading

--- src/Calc.js
+++ src/Calc.js
@@ -45,79 +45,82 @@ class Calc extends React.Component {
 
   render() {
     return (
-      <Container className="calc">
-        <Row className="first">
-          <Col>{this.state.answer}</Col>
-        </Row>
-        <Row className="second">
-          <Col xs={6}>
-            <button onClick={this.clearScreen}>clear</button>
-          </Col>
-          <Col>
-            <button onClick={this.computeResult} className="equalto">
-              =
-            </button>
-          </Col>
-          <Col>
-            <button onClick={this.operator("+")} className="plus">
-              +
-            </button>
-          </Col>
-        </Row>
-        <Row className="third">
-          <Col>
-            <button onClick={this.operator("7")}>7</button>
-          </Col>
-          <Col>
-            <button onClick={this.operator("8")}>8</button>
-          </Col>
-          <Col>
-            <button onClick={this.operator("9")}>9</button>
-          </Col>
-          <Col>
-            <button onClick={this.operator("-")} className="minus">
-              -
-            </button>
-          </Col>
-        </Row>
-        <Row className="fourth">
-          <Col>
-            <button onClick={this.operator("4")}>4</button>
-          </Col>
-          <Col>
-            <button onClick={this.operator("5")}>5</button>
-          </Col>
-          <Col>
-            <button onClick={this.operator("6")}>6</button>
-          </Col>
-          <Col>
-            <button onClick={this.operator("*")} className="multiply">
-              x
-            </button>
-          </Col>
-        </Row>
-        <Row className="fifth">
-          <Col>
-            <button onClick={this.operator("1")}>1</button>
-          </Col>
-          <Col>
-            <button onClick={this.operator("2")}>2</button>
-          </Col>
-          <Col>
-            <button onClick={this.operator("3")}>3</button>
-          </Col>
-          <Col>
-            <button onClick={this.operator("/")} className="divide">
-              /
-            </button>
-          </Col>
-        </Row>
-        <Row className="sixth">
-          <Col>
-            <button onClick={this.operator("0")}>0</button>
-          </Col>
-        </Row>
-      </Container>
+      <div style={{ width: "100%" }}>
+        <h1>React Basic Calculator</h1>
+        <Container className="calc">
+          <Row className="first">
+            <Col>{this.state.answer}</Col>
+          </Row>
+          <Row className="second">
+            <Col xs={6}>
+              <button onClick={this.clearScreen}>clear</button>
+            </Col>
+            <Col>
+              <button onClick={this.computeResult} className="equalto">
+                =
+              </button>
+            </Col>
+            <Col>
+              <button onClick={this.operator("+")} className="plus">
+                +
+              </button>
+            </Col>
+          </Row>
+          <Row className="third">
+            <Col>
+              <button onClick={this.operator("7")}>7</button>
+            </Col>
+            <Col>
+              <button onClick={this.operator("8")}>8</button>
+            </Col>
+            <Col>
+              <button onClick={this.operator("9")}>9</button>
+            </Col>
+            <Col>
+              <button onClick={this.operator("-")} className="minus">
+                -
+              </button>
+            </Col>
+          </Row>
+          <Row className="fourth">
+            <Col>
+              <button onClick={this.operator("4")}>4</button>
+            </Col>
+            <Col>
+              <button onClick={this.operator("5")}>5</button>
+            </Col>
+            <Col>
+              <button onClick={this.operator("6")}>6</button>
+            </Col>
+            <Col>
+              <button onClick={this.operator("*")} className="multiply">
+                x
+              </button>
+            </Col>
+          </Row>
+          <Row className="fifth">
+            <Col>
+              <button onClick={this.operator("1")}>1</button>
+            </Col>
+            <Col>
+              <button onClick={this.operator("2")}>2</button>
+            </Col>
+            <Col>
+              <button onClick={this.operator("3")}>3</button>
+            </Col>
+            <Col>
+              <button onClick={this.operator("/")} className="divide">
+                /
+              </button>
+            </Col>
+          </Row>
+          <Row className="sixth">
+            <Col>
+              <button onClick={this.operator("0")}>0</button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
